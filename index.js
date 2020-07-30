608ff54233e786804ee1df09678dc5cdccfb9367
Thien Nguyen <thiennguyen15dt1@gmail.com>
2020-07-30 19:51:41 +0700

    Dog eat Cat

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var Dog = require('./Dog');
+var Cat = require('./Cat');
 
-var dog = new Dog('Tom');
+var dog = new Dog('Mug');
 dog.sayHi();
+
+var cat = new Cat('Tom');
+
+dog.eat(cat);
+console.log(dog);
+
