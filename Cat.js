c3804376a20accc7de1bab2df31d44605cf7f01e
Thien_tnu <thiennguyen15dt1@gmail.com>
2020-12-20 06:09:10 +0700

    Test fetch

--- Cat.js
+++ Cat.js
@@ -5,6 +5,7 @@ function Cat(name){
 var Mouse = require('./Mouse');
 Cat.prototype.eat = function(animal){
 	if(animal instanceof Mouse){
+		console.log("thien dep trai")
 		this.stomach.push(animal);
 	}else{
 		throw new Error('Cat can eat only mouse!');
